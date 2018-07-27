453f677a534ca137b327450eb9a42fcd5e3c370b
Binzee <binzeefox@outlook.com>
2018-07-27 17:03:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tongxiwen/collection/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tongxiwen.collection;
+
+import android.os.Bundle;
+
+import com.example.tongxiwen.collection.base.FoxActivity;
+
+public class MainActivity extends FoxActivity {
+
+
+    @Override
+    protected int onInflateLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void create(Bundle savedInstanceState) {
+
+    }
+}
