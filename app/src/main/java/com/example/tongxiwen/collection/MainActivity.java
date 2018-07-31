02477600e706430d289afb517ea51c1a02ec2488
Binzee <binzeefox@outlook.com>
2018-07-31 14:11:43 +0800

    将图片加载弹窗的住布局改为动态添加，不需要额外的xml布局，节约移植成本。

--- app/src/main/java/com/example/tongxiwen/collection/MainActivity.java
+++ app/src/main/java/com/example/tongxiwen/collection/MainActivity.java
@@ -1,11 +1,24 @@
 package com.example.tongxiwen.collection;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.tongxiwen.collection.base.FoxActivity;
+import com.example.tongxiwen.collection.views.ImagePickerBottomSheetDialog;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends FoxActivity {
 
+    private ImagePickerBottomSheetDialog dialog;
 
     @Override
     protected int onInflateLayout() {
@@ -14,6 +27,20 @@ public class MainActivity extends FoxActivity {
 
     @Override
     protected void create(Bundle savedInstanceState) {
+        dialog = ImagePickerBottomSheetDialog.get(MainActivity.this);
+        findViewById(R.id.button_field).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        File file = dialog.onResult(requestCode, resultCode, data);
+        ImageView view = findViewById(R.id.image_field);
+        if (file != null)
+            view.setImageURI(Uri.fromFile(file));
     }
 }
